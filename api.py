8968b73e06fed03e5ff586874193dafc00ebf3ae
lucas-guimaraes_voda <lucas.guimaraes@vodafone.com>
2026-04-15 14:42:52 +0100

    multi-turn conversations

--- api.py
+++ api.py
@@ -6,29 +6,42 @@ load_dotenv()
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC")
 
+model = "claude-sonnet-4-6"
 client = Anthropic(api_key=ANTHROPIC_API_KEY)
-message = client.messages.create(
-    model="claude-sonnet-4-6",
-    max_tokens=1024,
-    messages=[
-        {
-          "role": "user", 
-          "content": "Define quantum computing in one sentence"
-        }
-    ],
-)
 
-print(message)
+def add_user_message(messages, text):
+    messages.append({
+        "role": "user",
+        "content": text
+    })
 
-message = client.messages.create(
-    model="claude-sonnet-4-6",
-    max_tokens=1024,
-    messages=[
-        {
-          "role": "user", 
-          "content": "Write another sentence"
-        }
-    ],
-)
+def add_assistant_message(messages, text):
+    messages.append({
+        "role": "assistant",
+        "content": text
+    })
 
-print(message)
+def chat(messages):
+    message = client.messages.create(
+        model=model,
+        max_tokens=1024,
+        messages=messages,
+    )
+    return message.content[0].text
+
+def main():
+    messages = []
+
+    add_user_message(messages, "Define quantum computing in one sentence")
+    answer = chat(messages)
+    print(answer)
+
+    add_assistant_message(messages, answer)
+    print(messages)
+
+    add_user_message(messages, "Write another sentence")
+    answer = chat(messages)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
