13de5207e01641bea0b0f3426ec63173c7ec9b37
guimatech <guimaraes-lucas@outlook.com.br>
2026-04-17 16:32:56 +0100

    steaming

---
+++ 004_streaming.py
@@ -0,0 +1,21 @@
+from common import client, model, add_user_message, chat
+
+def main():
+    messages = []
+    add_user_message(messages, "Write a 1 sentence description of a fake database")
+
+    with client.messages.create({
+        "model": model,
+        "max_tokens": 1024,
+        "messages": messages,
+        "stream": True
+    }) as stream:
+        for text in stream.text_stream:
+            print(text, end="")
+            # pass
+
+    # stream.get_final_message()
+
+
+if __name__ == "__main__":
+    main()
